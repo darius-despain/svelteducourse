7e6eaa75777e1383773d0892f60220ffba67ba45
Darius DeSpain <darius.despain.1@spaceforce.mil>
2023-12-13 20:01:15 -0700

    initial app created with image and some text

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,12 @@
 <script>
-  // let name = 'Svelte';
-  // let src = 'https://uploads.dailydot.com/2018/07/yoda-star-wars-meme.jpg?auto=compress&fm=pjpg'
-  // let string = 'Some <strong>string</strong> with html.';
+  let name = 'Svelte';
+  let src = 'https://uploads.dailydot.com/2018/07/yoda-star-wars-meme.jpg?auto=compress&fm=pjpg'
+  let string = 'Some <strong>string</strong> with html.';
 </script>
 
-<!-- <img {src} alt="Some text {name}" width="500px"/>
+<img {src} alt="Some text {name}" width="500px"/>
 <h1>Hello {name.toLowerCase()}</h1>
-<p>{@html string}</p> -->
-
-
+<p>{@html string}</p>
 
 <style>
   h1 {
